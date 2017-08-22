07c7da7d80408f57b525d597c95cf2bc459cc167
JARS32 <31205940+JARS32@users.noreply.github.com>
2017-08-22 18:58:22 +1000

    Pillars!!!!

---
+++ RRR/Ragedy Rampaging Raiders/Assets/Scripts/Gravity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float gravity;
+    private float accel;
+    private float accelStorage;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        accel = 1;
+        accelStorage = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(transform.up * gravity * accelStorage * -1, ForceMode.Acceleration);
+        accel += accelStorage;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        accel = accelStorage;
+    }
+}
